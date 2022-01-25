58a573ef4d383469a58da69b386865bd191756fb
JAI <jaikrishnan.devaraj@gmail.com>
2022-01-25 17:46:24 +0530

    today task

--- EMS-SYSTEM/src/app/app-routing.module.ts
+++ EMS-SYSTEM/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { HomeComponent } from './home/home.component';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {path:"login", component:LoginComponent},
@@ -13,6 +14,7 @@ const routes: Routes = [
   {path:'signup',component:SignupComponent},
   {path:'datas',component:DatasComponent},
   {path:'attendence',component:AttendenceComponent},
+  {path:'register',component:RegisterComponent},
   {path:'contact',component:ContactComponent},
   {path:'',redirectTo:'/home',pathMatch:'full'},
   {path:'**',component:HomeComponent}
